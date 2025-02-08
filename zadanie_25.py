80f7e53dcc0113b31a6cc86112f379e4651b1624
Artur Czarnota <czartur@gmail.com>
2025-02-08 17:48:54 +0100

    Update zadanie_25.py

--- zadanie_25.py
+++ zadanie_25.py
@@ -1,11 +1,11 @@
-# Pobieraj w pętli liczby całkowite z klawiatury, wkładaj je do listy V. Szczegółowe 
-# postępowanie we wnętrzu pętli to następujące kroki:  
-# 1. Pobierz liczbę i wprowadź ją na koniec listy.  
-# 2. Pobierz liczbę i wprowadź ją na koniec listy (tak, takie samo polecenie 
-# w ramach kroku pętli) [1]. 
-# 3. Jeżeli iloczyn dwóch ostatnich liczb z listy nie przekracza 1000, wprowadź 
+# Pobieraj w pętli liczby całkowite z klawiatury, wkładaj je do listy V. Szczegółowe
+# postępowanie we wnętrzu pętli to następujące kroki:
+# 1. Pobierz liczbę i wprowadź ją na koniec listy.
+# 2. Pobierz liczbę i wprowadź ją na koniec listy (tak, takie samo polecenie
+# w ramach kroku pętli) [1].
+# 3. Jeżeli iloczyn dwóch ostatnich liczb z listy nie przekracza 1000, wprowadź
 # również ten iloczyn do listy V i wróć do punktu 1., a jeżeli ten iloczyn przekro
-# czył wartość 1000, zakończ pętlę [1]. 
+# czył wartość 1000, zakończ pętlę [1].
 # Pokaż listę po wyjściu z pętli.
 
 V = []
